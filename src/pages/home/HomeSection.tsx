b1929cb6e5b0f80a8387a77725def5dec554baa1
Jiacheng Jiang <ajax15601560911@gmail.com>
2022-03-18 13:56:31 +0800

    Added spline model

--- src/pages/home/HomeSection.tsx
+++ src/pages/home/HomeSection.tsx
@@ -1,30 +1,25 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
 import { Section } from '../../components/Section';
+import Spline from '@splinetool/react-spline';
 
 export const HomeSection: FC = () => {
   return (
     <Section id="home">
       <Container>
         <PortraitContainer>
-          <PortraitImg src="assets/portrait.svg" />
+          <Spline scene="/assets/scene.spline" />
+          {/* <PortraitImg src="assets/portrait.svg" /> */}
         </PortraitContainer>
-        <div
-          className=""
-          style={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            gap: 10,
-          }}
-        >
-          <Name>Ajax (Jiacheng) Jiang</Name>
-          <Tag inverse>Suzhou, China</Tag>
-          <Tag>Software Developer</Tag>
-          <Tag>UI/UX Designer</Tag>
-          <Tag>Cat Owner</Tag>
-        </div>
+        <Info>
+          <div className="container">
+            <Name>Ajax (Jiacheng) Jiang</Name>
+            <Tag inverse>Suzhou, China</Tag>
+            <Tag>Software Developer</Tag>
+            <Tag>UI/UX Designer</Tag>
+            <Tag>Cat Owner</Tag>
+          </div>
+        </Info>
       </Container>
     </Section>
   );
@@ -34,6 +29,51 @@ const Name = styled.span`
   font-size: 24px;
   font-weight: bold;
   color: #333;
+`;
+
+const Info = styled.div`
+  @keyframes slidein {
+    from {
+      margin-top: 10px;
+      opacity: 0;
+      transform: scale(0.9);
+    }
+
+    to {
+      margin-top: 0px;
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  margin-top: 200px;
+
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  justify-content: flex-start;
+  gap: 10px;
+  /* position: 'absolute'; */
+  /* left: 50%; */
+  .container > * {
+    opacity: 0;
+
+    animation-delay: 1.5s;
+    animation-duration: 1s;
+    animation-name: slidein;
+    animation-fill-mode: forwards;
+  }
+
+  .container {
+    width: 300px;
+    height: 700px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    gap: 10px;
+    margin-left: 300px;
+  }
 `;
 
 const Tag: FC<{ inverse?: boolean }> = ({ inverse, children }) => {
@@ -48,6 +88,7 @@ const Tag: FC<{ inverse?: boolean }> = ({ inverse, children }) => {
 const TagBase = styled.div<{ inverse?: boolean }>`
   box-sizing: content-box;
   position: relative;
+  width: 250px;
 
   .content {
     user-select: none;
@@ -106,9 +147,25 @@ const PortraitImg = styled.img`
 `;
 
 const PortraitContainer = styled.div`
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
   height: 100%;
   display: flex;
   flex: 1;
   justify-content: flex-end;
   align-items: flex-end;
+  position: absolute;
+  opacity: 0;
+
+  animation-duration: 1s;
+  animation-delay: 0.5s;
+  animation-name: fade;
+  animation-fill-mode: forwards;
 `;
